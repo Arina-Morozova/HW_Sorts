efc9cb76e2449ba15915703761b06598ba11ebbc
Neto test <aryamorozkova555@gmail.com>
2023-07-19 14:19:33 +0300

    Change TicketTimeComparator

--- src/main/java/org/example/ticket/TicketTimeComparator.java
+++ src/main/java/org/example/ticket/TicketTimeComparator.java
@@ -4,11 +4,14 @@ import java.util.Comparator;
 
 public class TicketTimeComparator implements Comparator<Ticket> {
 
+
     @Override
     public int compare(Ticket t1, Ticket t2) {
-        if ((t1.getTimeTo() - t1.getTimeFrom()) > (t2.getTimeTo() - t2.getTimeFrom())) {
+        int t1FlightTime = t1.getTimeTo() - t1.getTimeFrom();
+        int t2FlightTime = t2.getTimeTo() - t2.getTimeFrom();
+        if (t1FlightTime > t2FlightTime) {
             return 1;
-        } else if ((t1.getTimeTo() - t1.getTimeFrom()) < (t2.getTimeTo() - t2.getTimeFrom())) {
+        } else if (t1FlightTime < t2FlightTime) {
             return -1;
         } else {
             return 0;
